0f12be5cce62102cc43a8727887f4f98b0e228d1
hominjang <51106030+hominjang@users.noreply.github.com>
2022-03-21 19:06:13 +0900

    22/03/21 logo (toolbar, appbar) setting
    logo 작업 + logo 위치에 있는 부분 작업
    오늘 recycleview 작업하려고 했는데, 사전에 필요한 부분이 너무 많아서 공부 중

---
+++ springph/src/main/java/org/tukorea/pi/domain/StockVO2.java
@@ -0,0 +1,23 @@
+package org.tukorea.pi.domain;
+
+public class StockVO2 {
+	private String symbol;
+	private String table_name;
+	
+	
+	public String getSymbol() {
+		return symbol;
+	}
+	public void setSymbol(String symbol) {
+		this.symbol = symbol;
+	}
+	public String getTable_name() {
+		return table_name;
+	}
+	public void setTable_name(String table_name) {
+		this.table_name = table_name;
+	}
+	
+	
+	
+}
--- springph/src/main/java/org/tukorea/pi/service/StockServiceImpl.java
+++ springph/src/main/java/org/tukorea/pi/service/StockServiceImpl.java
@@ -1,12 +1,17 @@
 package org.tukorea.pi.service;
 
 
-import java.time.LocalTime;
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Service;
 import org.tukorea.pi.domain.StockVO;
+import org.tukorea.pi.domain.StockVO2;
 import org.tukorea.pi.persistence.StockDAO;
 
 
@@ -15,18 +20,53 @@ import org.tukorea.pi.persistence.StockDAO;
 public class StockServiceImpl implements StockService {
 	@Autowired
 	private StockDAO stockDAO;
-
-	public StockVO readNowStock(String symbol) throws Exception{
-		LocalTime now = LocalTime.now();
-		int hour = now.getHour();
-		
-		// 장 개장시간은 13시부터 21시까지
-		if(hour>=13 && hour<21) {
-			return stockDAO.selectNowStock(symbol);
+	
+	public final List<String> list = new ArrayList<String>(10) {
+		{
+			add("117"); add("221");  add("415"); add("530"); add("620");
+			add("74"); add("95");  add("1124"); add("1125"); add("1226");
 		}
-		else {
+	};
+	
+	
+	public StockVO readNowStock(String symbol) throws Exception{
+		LocalDateTime now = LocalDateTime.now();
+		now.minusDays(24);
+		
+		String date = "stock_" + now.minusDays(24).format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+		
+		int hour = now.getHour();
+		String monthday =  Integer.toString(now.getMonthValue()) + Integer.toString(now.getDayOfMonth());
+		
+		StockVO2 stockVO2 = new StockVO2();
+		stockVO2.setSymbol(symbol);
+		stockVO2.setTable_name(date);
+
+		// 월요일이 1
+		DayOfWeek dayOfWeek = now.getDayOfWeek();
+		int dayOfWeekNumber = dayOfWeek.getValue();
+		// 0이면 개장일 1이면 휴장일
+		int flag = 0;
+		
+		//평일인경우
+		if(dayOfWeekNumber!=6 || dayOfWeekNumber!=7) {
+			// 장 개장시간은 13시부터 21시까지
+			for(int i=0;i<list.size();i++) {
+				if(monthday.equals(list.get(i))) {
+					flag = 1;
+					break;
+				}
+			}
+			if(hour>=13 && hour<21 && flag == 0) {
+				return stockDAO.selectNowStock(stockVO2);
+			}
+			else {
+				return stockDAO.selectLastStock(symbol);
+			}
+		}else {
 			return stockDAO.selectLastStock(symbol);
 		}
+
 
 	}
 	
