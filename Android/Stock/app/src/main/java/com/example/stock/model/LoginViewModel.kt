51c61f0c36890288283d3ce113f5f3dd9815d7f5
hominjang <51106030+hominjang@users.noreply.github.com>
2022-05-24 14:00:23 +0900

    22/05/24 LoginFragement Finished
    .

--- Android/Stock/app/src/main/java/com/example/stock/model/LoginViewModel.kt
+++ Android/Stock/app/src/main/java/com/example/stock/model/LoginViewModel.kt
@@ -1,6 +1,47 @@
 package com.example.stock.model
 
+import android.view.View
+import android.widget.Toast
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class LoginViewModel : ViewModel(){
+
+    private val _id = MutableLiveData<String>()
+    val id: LiveData<String>
+        get() = _id
+
+    private val _pwd = MutableLiveData<String>()
+    val pwd: LiveData<String>
+        get() = _pwd
+
+    fun onIdTextChanged(s: CharSequence) {
+        var b: String? = s.toString()
+
+        if (b != "") {
+            _id.value = b!!
+
+        } else {
+            _id.value = " "
+        }
+
+    }
+
+    fun onPwdTextChanged(s: CharSequence) {
+        var b: String? = s.toString()
+
+        if (b != "") {
+            _pwd.value = b!!
+
+        } else {
+            _pwd.value = " "
+        }
+
+    }
+
+
+    fun btnLoginClick(view : View) {
+        Toast.makeText(view.context, "id : "+ id.value + "pwd : " + pwd.value, Toast.LENGTH_SHORT).show()
+    }
 }
--- Android/Stock/app/src/main/java/com/example/stock/views/home/LoginFragment.kt
+++ Android/Stock/app/src/main/java/com/example/stock/views/home/LoginFragment.kt
@@ -5,16 +5,45 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.viewModels
 import com.example.stock.R
+import com.example.stock.databinding.FragmentLoginBinding
+import com.example.stock.model.LoginViewModel
 
 class LoginFragment : Fragment() {
-
+    lateinit var binding: FragmentLoginBinding
+    private val viewModel by viewModels<LoginViewModel>()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.viewModel = viewModel
+
+        viewModel.id.observe(viewLifecycleOwner, androidx.lifecycle.Observer {
+            if (binding.loginId.text.isNullOrEmpty()
+                &&
+                it.toString().isNotBlank()
+            ) {
+                binding.loginId.setText(it.toString())
+            }
+        })
+
+        viewModel.pwd.observe(viewLifecycleOwner, androidx.lifecycle.Observer {
+            if (binding.loginPwd.text.isNullOrEmpty()
+                &&
+                it.toString().isNotBlank()
+            ) {
+                binding.loginPwd.setText(it.toString())
+            }
+        })
     }
 
 
