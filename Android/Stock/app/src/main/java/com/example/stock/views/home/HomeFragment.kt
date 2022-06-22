43ee282c4041124db8a3a41788d3a37bc783f83e
hominjang <51106030+hominjang@users.noreply.github.com>
2022-06-22 16:06:46 +0900

    22/06/22 homeFragment arrange
    1. 주식 데이터 없을 때의 예외처리
    2. 뉴스, 랭킹 리사이클 뷰 항목 3개만 보여주는 형식으로 수정
    3. 주식 리사이클 뷰 항목 전체 보여주는 형식으로 수정
    4. 안쓰는 viewmodel들 수정 및 삭제

--- Android/Stock/app/src/main/java/com/example/stock/views/home/HomeFragment.kt
+++ Android/Stock/app/src/main/java/com/example/stock/views/home/HomeFragment.kt
@@ -1,32 +1,26 @@
 package com.example.stock.views.home
 
-import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintSet
+import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.NavController
-import androidx.navigation.NavDirections
 import androidx.navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.fragment.findNavController
 import com.example.stock.R
 import com.example.stock.adapter.NewsAdapter
 import com.example.stock.adapter.RankAdapter
 import com.example.stock.adapter.StockAdapter
 import com.example.stock.data.News
-import com.example.stock.databinding.FragmentFavoriteBinding
+import com.example.stock.data.Rank
 
 import com.example.stock.databinding.FragmentHomeBinding
 import com.example.stock.model.MainViewModel
-import com.example.stock.model.NewsViewModel
-import com.example.stock.model.StockViewModel
-import kotlinx.android.synthetic.main.activity_main.view.*
 
 class HomeFragment : Fragment() {
 
@@ -56,26 +50,70 @@ class HomeFragment : Fragment() {
 
         binding.homeFragment = this
 
-
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        Log.v("items", "여기들어옴0")
 
 //        binding.mainViewModel = mainViewModel
 
         mainViewModel.stockList.observe(viewLifecycleOwner, androidx.lifecycle.Observer {
+
+            val constraintLayout = binding.homeConstraint
+            val constraintSet = ConstraintSet()
+            constraintSet.clone(constraintLayout)
+
+            // 평상시 recycleview에 대이터가 있어서 있는 만큼 높이를 맞춰준거
+            binding.recyclerStockTextEmpty.isVisible = false
+            binding.recyclerStock.isVisible = true
+            constraintSet.connect(
+                binding.homeNews.id,
+                ConstraintSet.TOP,
+                binding.recyclerStock.id,
+                ConstraintSet.BOTTOM,
+                10
+            )
+
             stockAdapter.setData(it)
+
+            if (it.isEmpty()) {
+                // 소유한 주식이 하나도 없을 떄
+                // 주식이 없다는 textview가 뜨고, textview의 높이만큼 밑에 실시간 뉴스도 조정해줘야함
+                binding.recyclerStockTextEmpty.isVisible = true
+                binding.recyclerStock.isVisible = false
+
+                constraintSet.connect(
+                    binding.homeNews.id,
+                    ConstraintSet.TOP,
+                    binding.recyclerStockTextEmpty.id,
+                    ConstraintSet.BOTTOM,
+                    10
+                )
+                constraintSet.applyTo(constraintLayout)
+
+            }
         })
 
         mainViewModel.newsList.observe(viewLifecycleOwner, androidx.lifecycle.Observer {
-            newsAdapter.setData(it)
+            // 최신순으로 정렬하는 함수
+            // 최신순으로 3개만
+            var newsList : ArrayList<News> = arrayListOf()
+            for(i in 0..2){
+                newsList.add(it[i])
+            }
+            newsAdapter.setData(newsList)
         })
 
         mainViewModel.rankList.observe(viewLifecycleOwner, androidx.lifecycle.Observer {
-            rankAdapter.setData(it)
+            // 1,2,3위 순으로 정렬하는 함수
+            // 총 3개만 나오게끔
+            var rankList : ArrayList<Rank> = arrayListOf()
+            for(i in 0..2){
+                rankList.add(it[i])
+            }
+            rankAdapter.setData(rankList)
         })
 
     }
@@ -84,6 +122,7 @@ class HomeFragment : Fragment() {
         binding.scrollView.smoothScrollTo(0, binding.recyclerStock.bottom)
         Toast.makeText(activity, "Button Click", Toast.LENGTH_SHORT).show()
     }
+
     fun goRank(view: View) {
         binding.scrollView.smoothScrollTo(0, binding.recyclerNews.bottom)
         Toast.makeText(activity, "Button Click", Toast.LENGTH_SHORT).show()
@@ -103,11 +142,9 @@ class HomeFragment : Fragment() {
     }
 
 
-
-    fun search(view: View){
+    fun search(view: View) {
         view.findNavController().navigate(R.id.action_HomeFragment_to_searchFragment)
     }
-
 
 
     fun IMSI(view: View) {
