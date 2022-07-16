13b2e0bca128fef7b5b00177b3f170ddf2122adb
hominjang <51106030+hominjang@users.noreply.github.com>
2022-07-16 15:45:41 +0900

    22/07/16 Structural Improvement Required
    DB 개발 중에 구조개선 필요하다고 생각하여 설계 및 보수 중
    1. viewModel이 view에 의존적. (수정 가능)
    2. Event 처리 기능은 하지만, MVVM 패턴에 어울리지 않음. (시간 소요)
    - 코틀린 기본지식 / Event / Event Livedata
    3. 비동기 처리 적용하기 위해 밑작업 필요. (시간 소요)
    - 코루틴 / repositary

--- Android/Stock/app/src/main/java/com/example/stock/model/LoginViewModel.kt
+++ Android/Stock/app/src/main/java/com/example/stock/model/LoginViewModel.kt
@@ -1,14 +1,23 @@
 package com.example.stock.model
 
+import android.content.Context.MODE_PRIVATE
+import android.util.Log
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.navigation.findNavController
 import com.example.stock.R
+import com.example.stock.data.Auth
+import com.example.stock.data.SecureSharedPreferences
+import com.example.stock.data.retrofit.GlobalApplication
+import com.example.stock.data.retrofit.RetroAPI
+import retrofit2.Call
+import retrofit2.Response
 
-class LoginViewModel : ViewModel(){
+class LoginViewModel : ViewModel() {
 
     private val _id = MutableLiveData<String>()
     val id: LiveData<String>
@@ -22,6 +31,22 @@ class LoginViewModel : ViewModel(){
     val successLogin: LiveData<Boolean>
         get() = _successLogin
 
+    private val _errorText = MutableLiveData<String>()
+    val errorText: LiveData<String>
+        get() = _errorText
+
+//    1. 저장되어 있는 아이디랑 비밀번호가 있는지 확인
+//2. 없으면, 1)로 있으면 2)로
+//
+//1) 처음 로그인 할 때 / 일단
+//
+//2) 로그인 해서 키 / 아이디 / 비번 / sharedPrefernece있는 경우
+//get 요청 하려면 : 키 + 쿼리 필요함
+//1. 저장되어있는 아이디/비번으로 키받아와야함. (키업데이트)
+//2. 키 가지고 / user, stock 가져오기
+//
+//로그인은 동기.
+//stock 받아오는건 비동기.
 
     fun onIdTextChanged(s: CharSequence) {
         var b: String? = s.toString()
@@ -48,12 +73,30 @@ class LoginViewModel : ViewModel(){
     }
 
 
-    fun btnLoginClick(view : View) {
-        Toast.makeText(view.context, "id : "+ id.value + "pwd : " + pwd.value, Toast.LENGTH_SHORT).show()
+    fun btnLoginClick() {
+        var auth = Auth(id.value!!, pwd.value!!)
+        val call = GlobalApplication.baseService.create(RetroAPI::class.java)
+            .getUserKey(GlobalApplication.auth)
+        call.enqueue(object : retrofit2.Callback<String> {
+            override fun onResponse(call: Call<String>, response: Response<String>) {
+                Log.v("items", "HI response")
+                if (response.isSuccessful) {
+                    _successLogin.value = true
+//                    var key = response.body()!!
+//                    var sharedPrefs = getSharedPreferences("loginData", MODE_PRIVATE)
+//                    val secureSharedPreferences = SecureSharedPreferences.wrap(sharedPrefs)
+//                    secureSharedPreferences.put("key", key)
+                }
+            }
 
+            override fun onFailure(call: Call<String>, t: Throwable) {
+                Log.v("items", "HI Failure")
+            }
+
+        })
     }
 
-    fun btnSignup(view : View) {
+    fun btnSignup(view: View) {
         view.findNavController().navigate(R.id.action_loginFragment_to_signupFragment)
     }
 }
--- Android/Stock/app/src/main/java/com/example/stock/views/login/LoginFragment.kt
+++ Android/Stock/app/src/main/java/com/example/stock/views/login/LoginFragment.kt
@@ -5,8 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
+import androidx.navigation.findNavController
 import com.example.stock.R
 import com.example.stock.databinding.FragmentLoginBinding
 import com.example.stock.model.LoginViewModel
@@ -44,6 +46,22 @@ class LoginFragment : Fragment() {
                 binding.loginPwd.setText(it.toString())
             }
         })
+
+        viewModel.errorText.observe(viewLifecycleOwner, androidx.lifecycle.Observer {
+            Toast.makeText(view.context, it.toString(), Toast.LENGTH_SHORT)
+                .show()
+        })
+
+        viewModel.successLogin.observe(viewLifecycleOwner, androidx.lifecycle.Observer {
+
+            view.findNavController().navigate(R.id.action_loginFragment_to_HomeFragment)
+            //startDestionation 바꿔주기
+            // navGraph.startDestination = R.id.HomeFragment
+
+            // 회원가입으로 가버렷.
+            // view.findNavController().navigate(R.id.action_loginFragment_to_signupFragment)
+        })
+
     }
 
 
